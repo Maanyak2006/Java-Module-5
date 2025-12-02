30ea92b17c8c251afd04c6666027a617c6598893
Maanyak2006 <maanya62006@gmail.com>
2025-12-02 20:52:34 +0530

    Implement modulus operation with user input

---
+++ ModulusOperation.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class ModulusOperation {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        try {
+            System.out.print("Enter the first number: ");
+            int num1 = scanner.nextInt();
+
+            System.out.print("Enter the second number: ");
+            int num2 = scanner.nextInt();
+            int result = num1 % num2;
+            System.out.println("Result of " + num1 + " % " + num2 + " = " + result);
+
+        } catch (ArithmeticException e) {
+            System.out.println("Error: Cannot perform modulus operation with divisor zero.");
+        } finally {
+            scanner.close();
+        }
+    }
+}
